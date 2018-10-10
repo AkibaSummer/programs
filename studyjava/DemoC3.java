0e62563944b5af504aa3f7cf7d339cba85421a0c
akibasummer <oopsvpwowlq@gmail.com>
2018-10-10 07:30:31 +0000

    update

---
+++ studyjava/DemoC3.java
@@ -0,0 +1,14 @@
+package package02;
+
+public class DemoC3{    
+    private static viod ShowObjRef(){
+        int [] iArr = new int[10];
+        System.out.println(iArr);
+        iArr = null;//垃圾回收
+    }
+    
+    public static void main(String[] args){
+        ShowObjRef();
+        
+    }
+}
