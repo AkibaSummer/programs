68be7e80694e6d525f1d1be627b8d9889060baa8
akibasummer <oopsvpwowlq@gmail.com>
2018-10-28 11:22:40 +0000

    ds lab1

--- DS/Lab01_4.c
+++ DS/Lab01_4.c
@@ -6,19 +6,22 @@ typedef int bool;
 const int true=1;
 const int false=0;
 
-typedef struct node{
+typedef struct{
     int code;
     bool isDeleted;
-}
+}node;
 
-typedef struct SqList{
-    int *data;
+typedef struct{
+    node *data;
     int length;
-};
+}SqList;
 SqList root;
 
 void input(node* now,int num){
-    if (!num)return ;
+    if (!num){
+        puts("");
+        return ;
+    }
     scanf("%d",&(now->code));
     now->isDeleted=false;
     return input(now+1,num-1);
@@ -26,25 +29,23 @@ void input(node* now,int num){
 
 void output(node* now,int num,int n){
     if (!n)return ;
-    else if (now->data.isDeleted) output((now+1-root.data)%root.length+root.data,num,n);
-    else if (num==0){
+    else if (now->isDeleted) output((now+1-root.data)%root.length+root.data,num,n);
+    else if (!num){
         printf("%d ",now-root.data+1);
-        SqList* deleted=*now;
-        *now=(*now)->next;
-        if (n>1) output(&(deleted->next),deleted->val-1,n-1),free(deleted);
-        else puts("");
+        now->isDeleted=true;
+        output((now+1-root.data)%root.length+root.data,now->code-1,n-1);
     }
-    else output(&((*now)->next),num-1,n);
+    else output((now+1-root.data)%root.length+root.data,num-1,n);
 }
 
 int main(){
     int m;
     printf("请输入约瑟夫环人数（n）：");
     scanf("%d",&root.length);
-    root.data=(int*)malloc(n*sizeof(int));
+    root.data=(node*)malloc(root.length*sizeof(node));
     printf("请输入每个人所持有的密码值（n个数以空格隔开）：");
-	input(root.data,n);
+	input(root.data,root.length);
     printf("请输入初始密码值：");
     scanf("%d",&m);
-    output(pointToRoot,m-1,n);    
+    output(root.data,m-1,root.length);    
 }
