c207599d9703db40a19376ca9f0aaff9b2823030
akibasummer <oopsvpwowlq@gmail.com>
2018-11-01 07:29:04 +0000

    updata

--- DS/Lab02/Lab02_3.c
+++ DS/Lab02/Lab02_3.c
@@ -24,7 +24,7 @@ QElemType newQElemType(int a,int b){
 
 typedef struct ParkingCar{
     int license;
-    int arriveTime;
+    int arrivetime;
     int precost;
 }ParkingCar;
 
@@ -94,8 +94,10 @@ bool findCarInParking(int license){
 }
 
 bool findCarInSideWalk(int license){
-    for (int i=0;i<=ltop;i++){
-        if (license==parking[i].license)return true;
+    QueuePtr find=sidewalk.front;
+    while (find!=NULL){
+        if (find->data.license==license)return true;
+        find=find->next;
     }
     return false;
 }
@@ -105,7 +107,7 @@ void input(){
     scanf("%d",&parkingnum);
     char type;
     int license,time;
-    while (type!='E'){
+    while (type!='E'){	
         scanf("%c %d %d",&type,&license,&time);
         if (type=='A'){
             if (parkingnum==ltop+1) {
@@ -114,13 +116,39 @@ void input(){
             }
             else {
                 parking[++ltop]=newParkingCar(license,time,0);
-                printf("编号为 %d 的车停在便道的第 %d 个位置\n",license,ltop+1);                
+                printf("编号为 %d 的车停在停车场的第 %d 个位置\n",license,ltop+1);                
             }
         }
         else if (type=='D'){
-            
+            if (findCarInParking(license)){
+                while (parking[ltop].license!=license)
+                    parking[--rtop]=parking[ltop--];
+                printf("编号为 %d 的车已在便道上停放了 %d 个单位时间，已在停车场上停放了 %d 个单位的时间，共需缴纳 %d 个单位的费用\n",license,parking[ltop].precost,time-parking[ltop].arrivetime,parking[ltop].precost+(time-parking[ltop].arrivetime)*2);
+                --ltop;
+                while (rtop!=MAX_CAR_NUM)
+                    parking[++ltop]=parking[rtop++];
+                if (sidewalk.num){
+                    QElemType temp;
+                    DeQueue(&sidewalk,&temp);
+                    parking[++ltop]=newParkingCar(temp.license,time,time-temp.arrivetime);
+                }
+            }
+            else if (findCarInSideWalk(license)){
+                QElemType temp;
+                DeQueue(&sidewalk,&temp);
+                while (temp.license!=license){
+                    EnQueue(&sidewalk,temp);
+                    DeQueue(&sidewalk,&temp);
+                }printf("编号为 %d 的车已在便道上停放了 %d 个单位时间，共需缴纳 %d 个单位的费用",license,time-temp.arrivetime,time-temp.arrivetime);
+                
+                
+            }
+            else {
+                printf("无法找到编号为 %d 的车\n",license);
+            }
         }
     }
+    printf("谢谢使用\n");
 }
 void init(){
     InitQueue(&sidewalk);
@@ -130,5 +158,6 @@ void init(){
 
 
 int main(){
+    init();
     input();
 }
