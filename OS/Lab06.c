aae6c7ceeba3f2bbf8807285a0b02064df9b3d25
AkibaSummer <oopsvpwowlq@gmail.com>
2019-01-01 15:00:53 +0000

    update

--- OS/Lab06.c
+++ OS/Lab06.c
@@ -4,6 +4,7 @@
 #include <unistd.h>		//read|write|lseek所需头文件
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int getFunction(){
@@ -13,22 +14,23 @@ int getFunction(){
     puts("3、读文件");
     puts("4、修改文件权限");
     puts("5、查看当前文件权限并退出");
-    char ch[2];
+    char ch[10];
     scanf("%s",ch);
     while (ch[0]<'0'||ch[0]>'5'){
         puts("输入错误，请重新输入");
         scanf("%s",ch);
     }
+    return ch[0]-'0';
 }
 
-int getfile(int flags,mode_t mode){
+int getfile(mode_t flags){
     char a[256];
-    puts("请输入（或拖入）文件名或路径：");
+    printf("请输入（或拖入）文件名或路径：");
     for(;;){
-        gets(a);
-        int ret=open(a,flags,mode);
+        scanf("%s",a);
+        int ret=open(a,flags,0666);
         if (ret==-1){
-            puts("文件路径错误");
+            puts("发生错误");
         }
         else {
             return ret;
@@ -36,24 +38,59 @@ int getfile(int flags,mode_t mode){
     };
 }
 
-int func1(){
-    
+int func1(){   
+    close(getfile(O_RDWR|O_CREAT|O_EXCL));    
+    puts("创建成功！");
 }
 int func2(){
-    
+    int ret=getfile(O_WRONLY);
+    char buf[1024];
+    memset(buf,0,1024);
+    puts("请输入要写入的数据：");  
+    scanf("%s",buf);
+    int retnum=write(ret,buf,strlen(buf));
+    if (retnum==-1)puts("写入发生错误");
+    else printf("写入成功，buf=%s\n",buf);
+    close(ret);
+    return 0;
 }
 int func3(){
-    
+    int ret=getfile(O_RDONLY);
+    char buf[1024];
+    memset(buf,0,1024);
+    int retnum=read(ret,buf,1024);
+    if (retnum==-1)puts("读取发生错误");
+    else printf("读取成功，buf=%s\n",buf);
+    close(ret);
+    return 0;
 }
 int func4(){
-    
+    int ret=getfile(O_RDONLY);
+    puts("请输入要修改的文件权限：");
+    mode_t mode;
+    scanf("%o",&mode);
+    if (fchmod(ret,mode)==-1)
+        puts("修改失败");
+    else puts("修改成功");
+    close(ret);
+    return 0;    
 }
 int func5(){
-    
+    int ret=getfile(O_RDONLY);
+    struct stat a;
+    if (fstat(ret,&a)<0){
+        puts("发生错误");
+    }
+    else {
+        printf("所选文件权限为：%o\n",a.st_mode&07777);
+    }
+    close(ret);
+    return 0;
 }
 
 int main(){
     for (;;){
+        puts("");
         switch (getFunction()){
             case 0:
                 return 0;
@@ -71,7 +108,7 @@ int main(){
                 break;
             case 5:
                 func5();
-                return 0;
+                break;
         }
     }
 }
@@ -79,8 +116,7 @@ int main(){
 /*****************
 参考资料：
 https://www.linuxidc.com/Linux/2016-11/137612.htm
-
+https://blog.csdn.net/u014647208/article/details/5333731
 https://blog.csdn.net/ljianhui/article/details/10055665
-
 https://blog.csdn.net/shanshanpt/article/details/39852249
 *****************/
