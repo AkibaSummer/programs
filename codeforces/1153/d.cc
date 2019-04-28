7ad792b668c62e841f99f1c2f94237db369f53e5
AkibaSummer <oopsvpwowlq@gmail.com>
2019-04-28 23:42:33 +0800

    update

--- codeforces/1153/d.cc
+++ codeforces/1153/d.cc
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int maxx=300005;
+int n;
+int type[maxx];
+vector <int> node[maxx];
+int slove (int root){
+    if (node[root].empty()){
+        return 1;
+    }
+    else {
+        if (type[root]){
+            int ret=0x7fffffff; 
+            for (auto i:node[root]){
+                ret=min(ret,slove(i));
+            }
+            return ret;
+        }
+        else {
+            int ret=0;
+            for (auto i:node[root]){
+                ret+=slove(i);
+            }
+            return ret;
+        }
+    }
+}
 int main(){
-    
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        cin>>type[i];
+    }
+    for (int i=2;i<=n;i++){
+        int temp;
+        cin>>temp;
+        node[temp].push_back(i);
+    }
+    cout<<n-slove(1)<<endl;
 }
