ad09c495102dc09df29b24e2a148936d2806355e
AkibaSummer <oopsvpwowlq@gmail.com>
2019-12-04 11:08:47 +0800

    parallel lab02

---
+++ Parallel_Computing/Lab02.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+int main(){
+  sf::Window::create(sf::WindowHandle,)
+}
